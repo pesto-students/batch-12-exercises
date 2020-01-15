60655d2a1325d54cdd69a6de47b3f8e79a933b68
Viral Ruparel <viral@sysbioz.com>
2020-01-16 01:58:39 +0530

    rotatedString done

--- rotated-string/rotatedString.js
+++ rotated-string/rotatedString.js
@@ -1,6 +1,16 @@
 
-function rotatedString(...args) {
-  return args;
+function rotatedString(stringOne, stringTwo) {
+  if (stringOne.length != stringTwo.length) return false;
+
+  let clock_rot = ""
+  let anticlock_rot = ""
+  let l = stringTwo.length;
+
+  anticlock_rot = (anticlock_rot + stringTwo.substring(l - 2, l) + stringTwo.substring(0, l - 2));
+
+  clock_rot = clock_rot + stringTwo.substring(2) + stringTwo.substring(0, 2);
+
+  return (stringOne == clock_rot || stringOne == anticlock_rot);
 }
 
 export {
