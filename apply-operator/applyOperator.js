1081040c09461e217d468986595c9e1f71993140
ColdFire <fanilgandhi@gmail.com>
2020-01-15 14:56:50 +0530

    Completed applyOperator

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,25 @@
 
 function applyOperator(...args) {
-  return args;
+  const op = args.shift();
+  switch (op) {
+    case '+':
+      return args.reduce((a, b) => a + b, 0);
+
+    case '-':
+      return args.reduce((a, b) => a - b, 0);
+
+    case '*':
+      return args.reduce((a, b) => a * b, 1);
+
+    case '%':
+      return args.reduce((a, b) => a % b);
+
+    case '/':
+      return parseFloat(args.reduce((a, b) => a / b).toFixed(4));
+
+    default:
+      throw new Error('Operator is not valid');
+  }
 }
 
 export {
