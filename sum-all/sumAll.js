a314cf287042e10b88642ef94e4fb4861d8af498
Viral Ruparel <viral@sysbioz.com>
2020-01-16 02:12:28 +0530

    sumAll done

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,6 +1,16 @@
 
-function sumAll(...args) {
-  return args;
+function sumAll(inputArray) {
+  let resultSum = 0;
+  if (inputArray[0] < inputArray[1]) {
+    for (let i = inputArray[0]; i <= inputArray[1]; i++) {
+      resultSum += i;
+    }
+    return resultSum;
+  }
+  for (let i = inputArray[0]; i >= inputArray[1]; i--) {
+    resultSum += i;
+  }
+  return resultSum;
 }
 
 export {
