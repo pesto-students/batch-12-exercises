15494ab22a21ee2813dbc346700889d293925878
Viral Ruparel <viral@sysbioz.com>
2020-01-16 02:34:46 +0530

    convertToRoman done

--- convert-to-roman/convertToRoman.js
+++ convert-to-roman/convertToRoman.js
@@ -1,7 +1,18 @@
 
-function convertToRoman(...args) {
-  return args;
-}
+function convertToRoman(englishNumber) {
+  var decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var romanNumeral = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+  var romanized = "";
+
+  for (var i = 0; i < decimalValue.length; i++) {
+    while (decimalValue[i] <= englishNumber) {
+      romanized += romanNumeral[i];
+      englishNumber -= decimalValue[i];
+    }
+  }
+
+  return romanized;
+};
 
 export {
   convertToRoman,
