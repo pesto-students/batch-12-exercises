ad8d821126447ae12bcda0029732e70c4cc6bf17
ColdFire <fanilgandhi@gmail.com>
2020-01-15 18:53:54 +0530

    Completed factorial

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,6 +1,10 @@
 
-function factorial(...args) {
-  return args;
+function factorial(number) {
+  // factorial of 0 is 1
+  if (number === 0) {
+    return 1;
+  }
+  return number * factorial(number - 1);
 }
 
 export {
