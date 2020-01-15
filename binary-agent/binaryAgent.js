e269b7b269229fb9c6fe2380089ed14380f825af
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-15 23:24:39 +0530

    binaryagent done

--- binary-agent/binaryAgent.js
+++ binary-agent/binaryAgent.js
@@ -1,6 +1,9 @@
 
-function binaryAgent(...args) {
-  return args;
+function binaryAgent(binarystring) {
+  let binarylist = binarystring.split(' ');
+  // eslint-disable-next-line max-len
+  binarylist = binarylist.map((eachbinarystring) => String.fromCharCode(parseInt(eachbinarystring, 2)));
+  return binarylist.join('');
 }
 
 export {
