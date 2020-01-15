a3f1b59f48f5623fbdbf63d63b9a02263fa534a2
ColdFire <fanilgandhi@gmail.com>
2020-01-15 15:31:17 +0530

    Complete CounterFactory

--- counter-factory/counterFactory.js
+++ counter-factory/counterFactory.js
@@ -1,7 +1,17 @@
 
-function counterFactory(...args) {
-  return args;
-}
+const counterFactory = (function () {
+  let c = 0;
+  return {
+    increment: () => {
+      c += 1;
+      return c;
+    },
+    decrement: () => {
+      c -= 1;
+      return c;
+    },
+  };
+});
 
 export {
   counterFactory,
