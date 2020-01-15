41226154877cb3ca8c211ac3b8a031c623465d44
Viral Ruparel <viral@sysbioz.com>
2020-01-15 23:14:39 +0530

    longestWordInString Done

--- longest-word-in-string/longestWordInString.js
+++ longest-word-in-string/longestWordInString.js
@@ -1,6 +1,11 @@
 
-function longestWordInString(...args) {
-  return args;
+function longestWordInString(inputString) {
+  let wordsArray = inputString.split(' ');
+  let largestWordLength = 0;
+  for (let i = 0; i < wordsArray.length; i++) {
+    wordsArray[i].length > largestWordLength ? largestWordLength = wordsArray[i].length : largestWordLength;
+  }
+  return largestWordLength;
 }
 
 export {
