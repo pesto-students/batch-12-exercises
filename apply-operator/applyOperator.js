2da7cbe36d9d9fe40457d98a5dfd9d25a6841e95
Viral Ruparel <viral@sysbioz.com>
2020-01-15 16:05:37 +0530

    apply-operator half done

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,13 @@
 
-function applyOperator(...args) {
-  return args;
+function applyOperator(optr, ...args) {
+  const z = 0;
+  return optr === '+' ? args.reduce((acc, el) => acc + el, 0)
+    : optr === '-' ? args.reduce((acc, el) => acc - el, 0)
+      : optr === '*' ? args.reduce((acc, el) => acc * el, 1)
+        : optr === '/' ? Number(args.reduce((acc, el) => acc / el, 1).toFixed(4))
+          : optr === '%' ? args.reduce((acc, el) => acc % el)
+            : 0
+  // return;
 }
 
 export {
