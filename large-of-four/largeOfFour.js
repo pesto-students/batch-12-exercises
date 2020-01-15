a1d45d4c99a8623cbcf151034610e8f8460dd822
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-15 20:40:04 +0530

    largeoffour done

--- large-of-four/largeOfFour.js
+++ large-of-four/largeOfFour.js
@@ -1,6 +1,11 @@
 
-function largeOfFour(...args) {
-  return args;
+function largeOfFour(numberlists) {
+  const largeNumberList = [];
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < numberlists.length; i++) {
+    largeNumberList.push(Math.max(...numberlists[i]));
+  }
+  return largeNumberList;
 }
 
 export {
