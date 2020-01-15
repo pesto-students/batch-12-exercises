0dfd8c4a73945a2601d1d97fe0c44b06685b9fbb
Viral Ruparel <viral@sysbioz.com>
2020-01-16 02:21:06 +0530

    telephoneCheck done

--- telephone-check/telephoneCheck.js
+++ telephone-check/telephoneCheck.js
@@ -1,6 +1,7 @@
 
-function telephoneCheck(...args) {
-  return args;
+function telephoneCheck(phoneNumber) {
+  let usaNumberPattern = /^(1\s?)?((\([0-9]{3}\))|[0-9]{3})[\s\-]?[\0-9]{3}[\s\-]?[0-9]{4}$/;
+  return usaNumberPattern.test(phoneNumber);
 }
 
 export {
