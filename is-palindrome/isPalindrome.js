7ef8dc3ac5d225a8a5eeef32624abf932675dbda
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-15 20:21:47 +0530

    is-palindrome done

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,6 +1,9 @@
 
-function isPalindrome(...args) {
-  return args;
+function isPalindrome(value) {
+  const matchNonAlphanumeric = /[^A-Za-z0-9]/g;
+  const lowerCaseString = String(value).toLowerCase().replace(matchNonAlphanumeric, '');
+  const reversestring = lowerCaseString.split('').reverse().join('');
+  return reversestring === lowerCaseString;
 }
 
 export {
