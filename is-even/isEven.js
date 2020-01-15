4e3846d720405447720cfd52b362e0a88312ddc1
ColdFire <fanilgandhi@gmail.com>
2020-01-15 19:04:25 +0530

    Completed isEven

--- is-even/isEven.js
+++ is-even/isEven.js
@@ -1,6 +1,17 @@
 
-function isEven(...args) {
-  return args;
+function isEven(number) {
+  // If the last digit of the number is 0,2,4,6,8 then return true, else false
+  const lastdigit = number.toString().slice(-1);
+  switch (lastdigit) {
+    case '2':
+    case '4':
+    case '6':
+    case '8':
+    case '0':
+      return true;
+    default:
+      return false;
+  }
 }
 
 export {
