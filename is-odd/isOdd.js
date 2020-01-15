e3d69b79165ef5c04f45118a3362d70e44634c9e
ColdFire <fanilgandhi@gmail.com>
2020-01-15 19:06:07 +0530

    Completed isOdd

--- is-odd/isOdd.js
+++ is-odd/isOdd.js
@@ -1,6 +1,17 @@
 
-function isOdd(...args) {
-  return args;
+function isOdd(number) {
+  // If the last digit of the number is 1,3,5,7,9 then return true, else false
+  const lastdigit = number.toString().slice(-1);
+  switch (lastdigit) {
+    case '1':
+    case '3':
+    case '5':
+    case '7':
+    case '9':
+      return true;
+    default:
+      return false;
+  }
 }
 
 export {
