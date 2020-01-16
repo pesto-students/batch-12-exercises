92edb0cd1011bed9f8d5b9c39c87482e75faf291
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-16 21:08:45 +0530

    alternating done

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,6 +1,16 @@
 
-function alternatingCharacters(...args) {
-  return args;
+function alternatingCharacters(strings) {
+  const alternatingCharactersArray = strings.map((string) => {
+    const chararray = string.split('');
+    return chararray.reduce((count, curvalue, curindex) => {
+      if (curvalue === chararray[curindex + 1]) {
+        // eslint-disable-next-line no-param-reassign
+        count += 1;
+      }
+      return count;
+    }, 0);
+  });
+  return alternatingCharactersArray;
 }
 
 export {
