90344e7897500e91120a104b742656ca3a4e5f3d
ColdFire <fanilgandhi@gmail.com>
2020-01-16 14:47:04 +0530

    Completed Chunk-Array-In-Groups

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,6 +1,10 @@
 
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(inputArray, chunkSize) {
+  const answer = [];
+  for (let i = 0; i < inputArray.length; i += chunkSize) {
+    answer.push(inputArray.slice(i, i + chunkSize));
+  }
+  return answer;
 }
 
 export {
