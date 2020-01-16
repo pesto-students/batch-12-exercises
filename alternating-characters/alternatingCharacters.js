0f507ae09266bf22fd1a56f3210cdeb0b806aa75
Viral Ruparel <viral@sysbioz.com>
2020-01-16 19:48:26 +0530

    alternatingCharacters done

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,7 +1,23 @@
-
-function alternatingCharacters(...args) {
-  return args;
+function alternatingCharactersInString(string) {
+  const arrayOfCharacters = string.split('');
+  let count = 0;
+  arrayOfCharacters.reduce((acc, curr) => {
+    if (acc === curr) {
+      count += 1;
+    }
+    return curr;
+  });
+  return count;
 }
+
+function alternatingCharacters(array) {
+  const countArray = [];
+  return array.reduce((acc, curr) => {
+    countArray.push(alternatingCharactersInString(curr));
+    return countArray;
+  }, array[0]);
+}
+
 
 export {
   alternatingCharacters,
