883859a72ab272a75c44f9a09037ead2830440de
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-16 15:34:18 +0530

    findElement

--- find-element/findElement.js
+++ find-element/findElement.js
@@ -1,6 +1,8 @@
 
-function findElement(...args) {
-  return args;
+function findElement(inputArray,conditionFunction) {
+  const filteredArray = inputArray.filter(conditionFunction);
+  const firstElement = filteredArray[0];
+  return firstElement;
 }
 
 export {
