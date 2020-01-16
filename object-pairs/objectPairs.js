e9958c3b33781dce2be8f68d0e20566db6f17a9f
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-17 01:11:38 +0530

    object pair done

--- object-pairs/objectPairs.js
+++ object-pairs/objectPairs.js
@@ -1,6 +1,11 @@
 
-function objectPairs(...args) {
-  return args;
+function objectPairs(object) {
+  const objectPairCollection = [];
+  // eslint-disable-next-line guard-for-in
+  for (const key in object) {
+    objectPairCollection.push([key, object[key]]);
+  }
+  return objectPairCollection;
 }
 
 export {
