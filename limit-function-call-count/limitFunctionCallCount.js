4182f09c116f0b01e60f36726e4d0d9ea4210adb
ColdFire <fanilgandhi@gmail.com>
2020-01-16 12:01:27 +0530

    Complete limitFunctionCallCount

--- limit-function-call-count/limitFunctionCallCount.js
+++ limit-function-call-count/limitFunctionCallCount.js
@@ -1,6 +1,13 @@
 
-function limitFunctionCallCount(...args) {
-  return args;
+function limitFunctionCallCount(anyFunction, maxInvocationCounter) {
+  return function(...inputs) {
+    // decrement counter on function call
+    maxInvocationCounter -= 1;
+    if (maxInvocationCounter > 0) {
+      return anyFunction(...inputs);
+    }
+    return null;
+  };
 }
 
 export {
