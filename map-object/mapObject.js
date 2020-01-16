39f71d44134163a221dbfcf9cbc88309633f552b
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-16 20:17:13 +0530

    invert object done

--- map-object/mapObject.js
+++ map-object/mapObject.js
@@ -2,6 +2,7 @@
 function mapObject(numberObject, mapFunction) {
   // eslint-disable-next-line guard-for-in
   for (const key in numberObject) {
+    // eslint-disable-next-line no-param-reassign
     numberObject[key] = mapFunction(numberObject[key]);
   }
   return numberObject;
--- object-invert/objectInvert.js
+++ object-invert/objectInvert.js
@@ -1,6 +1,16 @@
 
-function objectInvert(...args) {
-  return args;
+function objectToKeyValueArrays(object) {
+  const objectkeys = Object.keys(object);
+  const objectvalues = Object.values(object);
+  return [objectkeys, objectvalues];
+}
+
+function objectInvert(object) {
+  const intervtedObject = {};
+  const [keyarray, valuearray] = objectToKeyValueArrays(object);
+  // eslint-disable-next-line no-return-assign
+  valuearray.forEach((key, i) => intervtedObject[key] = keyarray[i]);
+  return JSON.parse(JSON.stringify(intervtedObject));
 }
 
 export {
