b0a09522eca118c90a52ef10435498d228c85a14
Viral Ruparel <viral@sysbioz.com>
2020-01-16 08:11:12 +0530

    counter-factory done

--- counter-factory/counterFactory.js
+++ counter-factory/counterFactory.js
@@ -1,6 +1,17 @@
 
-function counterFactory(...args) {
-  return args;
+function counterFactory() {
+  let counterVariable = 0;
+  return {
+    increment: function () {
+      counterVariable++;
+      return counterVariable;
+    },
+    decrement: function () {
+      counterVariable--;
+      return counterVariable;
+    }
+  }
+
 }
 
 export {
