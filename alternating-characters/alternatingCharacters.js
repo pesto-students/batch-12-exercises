1b52e36276597801ab32c063ca4ebb97f6282351
ColdFire <fanilgandhi@gmail.com>
2020-01-16 19:03:28 +0530

    Completed Alternating Characters

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,6 +1,19 @@
 
-function alternatingCharacters(...args) {
-  return args;
+function getAlternatingCharsCount(string) {
+  const characterArray = string.split('');
+  let deletionsCount = 0;
+  characterArray.reduce((previousChar, currentChar) => {
+    if (previousChar === currentChar) {
+      deletionsCount += 1;
+      return previousChar;
+    }
+    return currentChar;
+  });
+  return deletionsCount;
+}
+
+function alternatingCharacters(array) {
+  return array.map(getAlternatingCharsCount);
 }
 
 export {
