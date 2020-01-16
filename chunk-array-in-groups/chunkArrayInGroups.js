ebad8bc3ee2b8e13f7d899ec3cf532ed10977538
Viral Ruparel <viral@sysbioz.com>
2020-01-16 14:57:55 +0530

    chunkArrayInGroups Done

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,6 +1,10 @@
 
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(inputArray, arraySize) {
+  const newArrayOfArray = [];
+  while (inputArray.length > 0) {
+    newArrayOfArray.push(inputArray.splice(0, arraySize));
+  }
+  return newArrayOfArray;
 }
 
 export {
