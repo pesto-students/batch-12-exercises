1927cee3b6b8b394efb87e43a940bccaf61e7e9c
Viral Ruparel <viral@sysbioz.com>
2020-01-16 07:14:28 +0530

    applyOperator done

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,13 +1,17 @@
 
-function applyOperator(optr, ...args) {
+function applyOperator(Operator, ...operands) {
   const z = 0;
-  return optr === '+' ? args.reduce((acc, el) => acc + el, 0)
-    : optr === '-' ? args.reduce((acc, el) => acc - el, 0)
-      : optr === '*' ? args.reduce((acc, el) => acc * el, 1)
-        : optr === '/' ? Number(args.reduce((acc, el) => acc / el, 1).toFixed(4))
-          : optr === '%' ? args.reduce((acc, el) => acc % el)
-            : 0
+  return Operator === '+' ? operands.reduce((acc, el) => acc + el, 0)
+    : Operator === '-' ? operands.reduce((acc, el) => acc - el, 0)
+      : Operator === '*' ? operands.reduce((acc, el) => acc * el, 1)
+        : Operator === '/' ? Number(operands.reduce((acc, el) => acc / el, 1).toFixed(4))
+          : Operator === '%' ? operands.reduce((acc, el) => acc % el)
+            : throwError();
   // return;
+}
+
+function throwError() {
+  throw new Error("Operator not found, Expected one of +, -, *, /, %");
 }
 
 export {
