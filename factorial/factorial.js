f23a48d253bbe06223e89c1d11fd890aa8d75309
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-18 00:52:09 +0530

    factorial

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,6 +1,12 @@
 
-function factorial(...args) {
-  return args;
+function factorial(number) {
+  if(parseInt(number)<0 || typeof number !== "number"){
+    throw new Error("The provided value is not a Number or less than 0.");
+  }
+  if(parseInt(number) === 1 || parseInt(number) === 0){
+    return 1;
+  }
+  return (parseInt(number) * factorial(parseInt(number)-1));
 }
 
 export {
