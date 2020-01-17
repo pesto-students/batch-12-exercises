078117c6e6e093d74ee5f2fba739eb906736bc62
ColdFire <fanilgandhi@gmail.com>
2020-01-17 15:29:19 +0530

    [WIP] BigNumbers

--- add-big-integers/addBigIntegers.js
+++ add-big-integers/addBigIntegers.js
@@ -1,6 +1,10 @@
 
-function addBigIntegers(...args) {
-  return args;
+function addBigIntegers(string) {
+  const array = string.split('\n');
+  const countOfIntegers = parseInt(array.shift());
+  const sum = array.reduce((sum, n) => (sum + parseInt(n)), 0);
+  return sum.toString();
+
 }
 
 export {
