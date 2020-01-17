a62385e95410de91c0e287a47d3e988eeb4df9c3
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-18 00:27:59 +0530

    counter

--- counter/counter.js
+++ counter/counter.js
@@ -1,6 +1,11 @@
 
-function counter(...args) {
-  return args;
+function counter() {
+  var count = 0;
+  const incrementCount = function(){
+    count+=1;
+    return count;
+  }
+  return incrementCount;
 }
 
 export {
