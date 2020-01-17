4543329c2b0066c1877cedd400f6387b17a3653c
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-18 01:29:29 +0530

    is-triangle

--- is-triangle/isTriangle.js
+++ is-triangle/isTriangle.js
@@ -1,6 +1,12 @@
 
-function isTriangle(...args) {
-  return args;
+function isTriangle(...sides) {
+  if (sides.length !== 3) {
+    throw new Error('Expected (3) lengths of the triangle');
+  }
+  const sideA = parseInt(sides[0]);
+  const sideB = parseInt(sides[1]);
+  const sideC = parseInt(sides[2]);
+  return (sideA<sideB+sideC && sideB<sideA+sideC && sideC<sideB+sideA)
 }
 
 export {
