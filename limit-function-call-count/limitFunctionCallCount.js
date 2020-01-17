01ebc6f0abfe98dca0038770cb715978dd0d9b74
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-18 01:40:10 +0530

    limit-function-call-count

--- limit-function-call-count/limitFunctionCallCount.js
+++ limit-function-call-count/limitFunctionCallCount.js
@@ -1,6 +1,14 @@
 
-function limitFunctionCallCount(...args) {
-  return args;
+function limitFunctionCallCount(callBackFunc, maxCallsOfFunc) {
+  var maxLimit = maxCallsOfFunc;
+  const limitedCallBackFunc = function (...params) {
+    if (maxLimit > 0) {
+      maxLimit -= 1;
+      return callBackFunc(...params);
+    }
+    return null;
+  }
+  return limitedCallBackFunc;
 }
 
 export {
