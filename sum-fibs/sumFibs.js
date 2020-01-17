4d813eaa083d83ae23a3d6b6ad3dc40a80756ba4
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-18 03:12:03 +0530

    sum-fibs/

--- sum-fibs/sumFibs.js
+++ sum-fibs/sumFibs.js
@@ -1,6 +1,22 @@
+const memoFibs = {};
 
-function sumFibs(...args) {
-  return args;
+function calcFib(number) {
+  if (number === 0 || number === 1) { return 1; }
+  if (memoFibs.hasOwnProperty(number)) { return memoFibs[number]; }
+  var calculatedFib = calcFib(number - 1) + calcFib(number - 2);
+  memoFibs[number] = calculatedFib;
+  return calculatedFib;
+}
+
+function sumFibs(number) {
+  var summationOfFibs = 0;
+  for (var initial = 0; calcFib(initial) < number; initial++) {
+    var fib = calcFib(initial);
+    if (fib % 2 !== 0) {
+      summationOfFibs += fib;
+    }
+  }
+  return summationOfFibs;
 }
 
 export {
