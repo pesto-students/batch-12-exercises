fecf309f959695eab03b0b371b90b9baacefe35b
ColdFire <fanilgandhi@gmail.com>
2020-01-17 08:36:17 +0530

    Complete sumFibs

--- sum-fibs/sumFibs.js
+++ sum-fibs/sumFibs.js
@@ -1,6 +1,30 @@
 
-function sumFibs(...args) {
-  return args;
+function fibonacciGenerator() {
+  let a = 0;
+  let b = 1;
+  return (() => {
+    b += a;
+    a = b - a;
+    return a;
+  });
+}
+
+function getOddFibonaciiUpto(uptoNumber) {
+  const fibonacciNumbers = [];
+  const getNextFibonaci = fibonacciGenerator();
+  let nextFibonacci = getNextFibonaci();
+  while (nextFibonacci < uptoNumber) {
+    fibonacciNumbers.push(nextFibonacci);
+    nextFibonacci = getNextFibonaci();
+  }
+  const oddFibonacciNumbers = fibonacciNumbers.filter((n) => (n % 2) !== 0);
+  return oddFibonacciNumbers;
+}
+
+function sumFibs(uptoNumber) {
+  const oddFibonacciNumbers = getOddFibonaciiUpto(uptoNumber);
+  const sumFibonacciNumbers = oddFibonacciNumbers.reduce((a, b) => (a + b), 0);
+  return sumFibonacciNumbers;
 }
 
 export {
