872049185e7e2579ee39ab8e181514786b53f388
ColdFire <fanilgandhi@gmail.com>
2020-01-17 07:54:35 +0530

    Complete cacheFunction

--- cache-function/cacheFunction.js
+++ cache-function/cacheFunction.js
@@ -1,6 +1,12 @@
 
-function cacheFunction(...args) {
-  return args;
+function cacheFunction(predicateFn) {
+  const cache = {};
+  return ((...args) => {
+    if (!(args in cache)) {
+      cache[args] = predicateFn(args);
+    }
+    return cache[args];
+  });
 }
 
 export {
