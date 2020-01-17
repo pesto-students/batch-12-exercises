5ed80369c613dfa4eb7dc4302fa4b6f8abfb2589
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-17 15:18:40 +0530

    abbreviate-string

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,14 @@
 
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(string) {
+  if(typeof string !== "string"){
+    throw string+" is not of type string.";
+  }
+  const stringAsArray = string.split(' ');
+  const firstName = stringAsArray[0];
+  const lastName = stringAsArray[stringAsArray.length-1];
+  const shortenLastName = lastName.slice(0,1).toUpperCase();
+  const abbreviateString = firstName+" "+shortenLastName+".";
+  return abbreviateString;
 }
 
 export {
