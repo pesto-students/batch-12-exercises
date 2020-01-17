a70288667b715fbe47ee63f8fd61096fc1e430b2
ColdFire <fanilgandhi@gmail.com>
2020-01-17 15:07:23 +0530

    Complete abbreviateString

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,16 @@
 
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(string) {
+  if ((typeof string) === 'String') {
+    throw new Error(`Received ${string} of type ${typeof string}. Expected a string.`);
+  }
+  const words = string.split(' ');
+  const firstWord = words[0];
+  const acronym = words[words.length - 1][0];
+
+  if (words.length > 1) {
+    return `${firstWord} ${acronym.toUpperCase()}.`;
+  }
+  return string;
 }
 
 export {
