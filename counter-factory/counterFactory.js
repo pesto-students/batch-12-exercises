d09da2e13753bb147c9d1d4a452ad71157dd6d37
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-18 00:31:00 +0530

    counter-factory

--- counter-factory/counterFactory.js
+++ counter-factory/counterFactory.js
@@ -1,6 +1,15 @@
 
-function counterFactory(...args) {
-  return args;
+function counterFactory() {
+  var count = 0;
+  const increment = function () {
+    count += 1;
+    return count;
+  }
+  const decrement = function () {
+    count -= 1;
+    return count;
+  }
+  return { increment, decrement };
 }
 
 export {
