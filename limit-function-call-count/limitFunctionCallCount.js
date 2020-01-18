5b142a8c7bc89a1d47637dedeff1355f4f3a0f3e
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-18 16:35:39 +0530

    limit-function-call-count

--- limit-function-call-count/limitFunctionCallCount.js
+++ limit-function-call-count/limitFunctionCallCount.js
@@ -1,8 +1,16 @@
 
-function limitFunctionCallCount(...args) {
-  return args;
+function limitFunctionCallCount(fn, fnnvokeCount) {
+  let invokeCount = fnnvokeCount;
+  const callbackFunction = function callbackFunction(...args) {
+    if (invokeCount) {
+      invokeCount -= 1;
+      return fn(...args);
+    }
+    return null;
+  };
+  return callbackFunction;
 }
 
 export {
   limitFunctionCallCount,
-};
+}
