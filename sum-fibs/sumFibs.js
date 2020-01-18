43700b5641c1423252963248dad069ee778866a8
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-18 17:24:44 +0530

    fibo done

--- sum-fibs/sumFibs.js
+++ sum-fibs/sumFibs.js
@@ -1,6 +1,22 @@
+function fibonacciSeries(number) {
+  const fiboArray = [1, 1];
+  let [, lastElement] = fiboArray;
+  while (lastElement < number) {
+    fiboArray.push(
+      fiboArray[fiboArray.length - 1] + fiboArray[fiboArray.length - 2],
+    );
+    lastElement = fiboArray[fiboArray.length - 1];
+  }
+  return fiboArray;
+}
 
-function sumFibs(...args) {
-  return args;
+function sumFibs(number) {
+  const fibonacciArray = fibonacciSeries(number);
+  const oddfibonacciArray = fibonacciArray.filter((no) => no % 2 !== 0 && no < number);
+  if (oddfibonacciArray.length) {
+    return oddfibonacciArray.reduce((a, b) => a + b);
+  }
+  return 0;
 }
 
 export {
