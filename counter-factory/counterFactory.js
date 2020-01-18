f2c7f28987be255fb8fc06df158356c06351673d
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-18 14:14:01 +0530

    counter-factory done

--- counter-factory/counterFactory.js
+++ counter-factory/counterFactory.js
@@ -1,6 +1,16 @@
 
-function counterFactory(...args) {
-  return args;
+function counterFactory() {
+  let step = 0;
+  return {
+    increment: () => {
+      step += 1;
+      return step;
+    },
+    decrement: () => {
+      step -= 1;
+      return step;
+    },
+  };
 }
 
 export {
