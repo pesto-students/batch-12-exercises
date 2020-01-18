b26fc6f5f0e5d0357f8abf22a1ad7379adbfdb3c
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-18 14:48:25 +0530

    is-triangle done

--- is-triangle/isTriangle.js
+++ is-triangle/isTriangle.js
@@ -1,7 +1,12 @@
 
-function isTriangle(...args) {
-  return args;
+function isTriangle(...number) {
+  number.sort((a, b) => a - b);
+  const [triangleside1, triangleside2, trianglebiggerside3] = number;
+  return triangleside1 ** 2 + triangleside2 ** 2 === trianglebiggerside3 ** 2;
 }
+
+// isTriangle(5, 3, 4);
+
 
 export {
   isTriangle,
