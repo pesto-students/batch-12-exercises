8e299c7ae779b16870f35e22492b9a8b23d5d58c
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-18 17:37:19 +0530

    three-sum

--- three-sum/threeSum.js
+++ three-sum/threeSum.js
@@ -1,6 +1,28 @@
 
-function threeSum(...args) {
-  return args;
+function threeSum(numbers, sumOfTriplet) {
+
+  if (numbers.length < 3) {
+    throw new Error('Length of the Array is less than 3.');
+  }
+
+  if (typeof sumOfTriplet !== "number") {
+    throw new Error('Expected Sum as \"Number\". Recieved Type : ' + typeof sumOfTriplet);
+  }
+
+  const sizeOfNmbers = numbers.length;
+  for (var i = 0; i < sizeOfNmbers - 2; i++) {
+    var numA = numbers[i];
+    for (var j = 1; j < sizeOfNmbers - 1; j++) {
+      var numB = numbers[j];
+      for (var k = 2; k < sizeOfNmbers; k++) {
+        var numC = numbers[k];
+        if (numA + numB + numC === sumOfTriplet) {
+          return [numA, numB, numC];
+        }
+      }
+    }
+  }
+  return null;
 }
 
 export {
