78b8a6021e71fc86cc281c80e3b6fb96e54e8aa9
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-18 16:45:13 +0530

    sum-primes

--- sum-primes/sumPrimes.js
+++ sum-primes/sumPrimes.js
@@ -1,6 +1,26 @@
 
-function sumPrimes(...args) {
-  return args;
+function isPrime(number) {
+  var isPrimeNumber = true;
+  for (var divisor = 2; divisor < number; divisor++) {
+    if (number % divisor === 0) { isPrimeNumber = false; }
+  }
+  return isPrimeNumber;
+}
+
+function getPrimeNumbers(number) {
+  var primeNumbers = [];
+  for (var initial = 2; initial <= number; initial++) {
+    if(isPrime(initial)){
+      primeNumbers.push(initial);
+    }
+  }
+  return primeNumbers;
+}
+
+function sumPrimes(number) {
+  const arrayOfPrimeNumbers = getPrimeNumbers(number);
+  const sumOfPrimeNumbers = arrayOfPrimeNumbers.reduce((sum,primeNumber)=>sum+primeNumber,0);
+  return sumOfPrimeNumbers;
 }
 
 export {
