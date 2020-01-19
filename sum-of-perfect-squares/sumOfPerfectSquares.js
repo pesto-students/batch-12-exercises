d39edcf58152c2515f7588988bceaa95ea1ebc0f
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-20 01:17:26 +0530

    remaining in progress

--- sum-of-perfect-squares/sumOfPerfectSquares.js
+++ sum-of-perfect-squares/sumOfPerfectSquares.js
@@ -1,8 +1,24 @@
+const smallestlistofPerfectSquare = [];
 
-function sumOfPerfectSquares(...args) {
-  return args;
+function arrayofsmallsquare(number) {
+  if (number === 1) {
+    smallestlistofPerfectSquare.push(1);
+    return smallestlistofPerfectSquare;
+  }
+  let temp = Math.floor(Math.sqrt(number));
+  smallestlistofPerfectSquare.push(temp);
+  var diff = number - (temp * temp);
+  return arrayofsmallsquare(diff);
+}
+
+function sumOfPerfectSquares(number) {
+  arrayofsmallsquare(number);
+  return smallestlistofPerfectSquare.length;
 }
 
 export {
   sumOfPerfectSquares,
 };
+
+
+// console.log(sumOfPerfectSquares(15));
--- three-sum/threeSum.js
+++ three-sum/threeSum.js
@@ -1,5 +1,12 @@
 
-function threeSum(...args) {
+function threeSum(numberArray, targetNumber) {
+  if (numberArray.length < 3) {
+    throw new Error('Number Array doesn\'t have the required size');
+  }
+  if (typeof targetNumber !== 'number') {
+    throw new Error('targetNumber is not a number');
+  }
+
   return args;
 }
 
