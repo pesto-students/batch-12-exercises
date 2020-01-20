4a668447d8f96ee8a5273f8dae4161d0f287a39c
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-20 20:27:34 +0530

    timeout done

--- timeout/timeout.js
+++ timeout/timeout.js
@@ -1,6 +1,12 @@
 
-function timeout(...args) {
-  return args;
+function timeout(name) {
+  const p = new Promise((res, rej) => {
+    if (!name) {
+      rej(new Error('no input given'));
+    }
+    setTimeout(() => res(`Hello ${name}`), 300);
+  });
+  return p;
 }
 
 export {
