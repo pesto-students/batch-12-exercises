d240a983bb69be5286f34b296ce4f5b0f222c3f3
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-20 15:25:34 +0530

    double-odd-number

--- double-odd-numbers/doubleOddNumbers.js
+++ double-odd-numbers/doubleOddNumbers.js
@@ -1,6 +1,8 @@
 
-function doubleOddNumbers(...args) {
-  return args;
+function doubleOddNumbers(array) {
+  let oddNumbersArray = array.filter((eachNum) => eachNum % 2 != 0 ? true : false);
+  let doubleOddNumbers = oddNumbersArray.map((eachNum) => eachNum * 2);
+  return doubleOddNumbers;
 }
 
 export {
