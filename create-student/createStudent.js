ebef8f8616a1b7af540735226717d1f5eecd5f4b
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-20 15:21:25 +0530

    create-student

--- create-student/createStudent.js
+++ create-student/createStudent.js
@@ -1,6 +1,26 @@
 
-function createStudent(...args) {
-  return args;
+function createStudent(props = {}) {
+  const {
+    likesJavaScript : childLikesJS = true,
+    likesES2015 : childLikesES2015 = true
+  } = props;
+  var baseStatement = "The student likes ";
+  if(childLikesJS){
+    baseStatement += "JavaScript";
+  }
+  if(childLikesES2015 === true && childLikesJS === true){
+    baseStatement += " and ";
+  }
+  if(childLikesES2015){
+    baseStatement += "ES2015"; 
+  }
+  if(childLikesES2015 != childLikesJS){
+    baseStatement += "!";
+  }
+  if(childLikesJS === false && childLikesES2015 === false){
+    return "The student does not like much...";
+  }
+  return baseStatement;
 }
 
 export {
