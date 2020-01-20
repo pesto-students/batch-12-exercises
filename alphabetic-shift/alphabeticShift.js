f0254919ff4b3c12247f377fc91a053c3bb20ff7
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-20 22:41:07 +0530

    alphabetic-shift

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,14 @@
 
-function alphabeticShift(...args) {
-  return args;
+var shiftToNextChar = (char) => {
+  let charCode = char.charCodeAt(0);
+  charCode += 1;
+  return String.fromCharCode(charCode);
+}
+
+function alphabeticShift(string) {
+  const chars = string.split('');
+  const shiftedCharsArray = chars.map(shiftToNextChar);
+  return shiftedCharsArray.join('');
 }
 
 export {
