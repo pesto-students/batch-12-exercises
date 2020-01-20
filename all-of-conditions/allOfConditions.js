14f1ae81d5cbf4fab996d7548a022ea05abed404
ColdFire <fanilgandhi@gmail.com>
2020-01-20 15:59:43 +0530

    Complete allOfConditions

--- all-of-conditions/allOfConditions.js
+++ all-of-conditions/allOfConditions.js
@@ -1,8 +1,16 @@
 
-function allOfConditions(...args) {
-  return args;
-}
-
+const allOfConditions = (...callbackFunctions) => ((input) => {
+  if (typeof callbackFunctions === 'function') {
+    return callbackFunctions(input);
+  }
+  for (const callbackFunction of callbackFunctions) {
+    const result = callbackFunction(input);
+    if (Boolean(result) === false) {
+      break;
+    }
+  }
+  return null;
+});
 export {
   allOfConditions,
 };
