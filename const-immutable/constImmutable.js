bffcc27503f82b456383b032b23db2bf47aa9966
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-20 15:03:08 +0530

    const-immutable

--- const-immutable/constImmutable.js
+++ const-immutable/constImmutable.js
@@ -1,6 +1,13 @@
 
-function constImmutable(...args) {
-  return args;
+function constImmutable() {
+  const account = Object.freeze({
+    username: 'pesto',
+    password: 'initialPassword',
+    });
+  try{
+    account.password = 's3cret';
+  }catch(e){}
+  return account.password;
 }
 
 export {
