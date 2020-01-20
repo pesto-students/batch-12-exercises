3d35a1fb0fe95511aaa168e0ffe0b50cc8ea92b5
Viral Ruparel <viral@sysbioz.com>
2020-01-21 00:32:20 +0530

    alphabeticShift Done

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,8 @@
 
-function alphabeticShift(...args) {
-  return args;
+function alphabeticShift(string) {
+  const tokenizedString = string.split('');
+  const shiftedArray = tokenizedString.map((chr) => String.fromCharCode(chr.charCodeAt(0) + 1));
+  return shiftedArray.join('');
 }
 
 export {
