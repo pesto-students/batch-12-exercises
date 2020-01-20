b7bd2afbc8843bbcf0d245f6ae03d10d07362a06
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-20 23:28:54 +0530

    promise-all-props

--- promise-all-props/promiseAllProps.js
+++ promise-all-props/promiseAllProps.js
@@ -1,6 +1,12 @@
 
-function promiseAllProps(...args) {
-  return args;
+
+function promiseAllProps(promiseObject = {}) {
+  const keys = Object.keys(promiseObject);
+  const resolved = {};
+  for (const key of keys) {
+    promiseObject[key].then(value => { resolved[key] = value });
+  }
+  return Promise.resolve(resolved);
 }
 
 export {
