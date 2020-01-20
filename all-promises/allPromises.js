7d687636a1edab0077ee4f1ee597205a42f17438
Viral Ruparel <viral@sysbioz.com>
2020-01-21 00:02:37 +0530

    allPromises done

--- all-promises/allPromises.js
+++ all-promises/allPromises.js
@@ -1,8 +1,11 @@
-
-function allPromises(...args) {
-  return args;
-}
-
+const allPromises = (promises) => new Promise((res) => {
+  const allResolve = [];
+  for (const promise of promises) {
+    const resolved = Promise.resolve(promise);
+    resolved.then((value) => allResolve.push(value));
+  }
+  return res(allResolve);
+});
 export {
   allPromises,
 };
