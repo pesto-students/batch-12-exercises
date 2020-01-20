117b8bbf31d05d1fd40f76fba4ca497ab112b51a
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-20 23:47:22 +0530

    all promise done

--- all-promises/allPromises.js
+++ all-promises/allPromises.js
@@ -1,6 +1,12 @@
-
-function allPromises(...args) {
-  return args;
+function allPromises(array = []) {
+  const promiseResolveArray = [];
+  for (const item of array) {
+    const promise = Promise.resolve(item);
+    promise.then((value) => {
+      promiseResolveArray.push(value);
+    });
+  }
+  return Promise.resolve(promiseResolveArray);
 }
 
 export {
