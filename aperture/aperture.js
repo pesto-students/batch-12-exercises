91d7673e99e7c9d69a4d401c77c20a2c72387bc3
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-22 00:12:30 +0530

    object-assign

--- aperture/aperture.js
+++ aperture/aperture.js
@@ -1,12 +1,12 @@
 
-const splice = (start, end, array) => {
+const slice = (start, end, array) => {
   return array.slice(start, end);
 }
 
 function aperture(chunkSize, array) {
   const result = [];
   for (let index = 0; index <= array.length - chunkSize; index += 1) {
-    const chunk = splice(index, index + chunkSize, array)
+    const chunk = slice(index, index + chunkSize, array)
     result.push(chunk);
   }
   return result;
