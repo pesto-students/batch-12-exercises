72bd42533cb6490e4686c0c5c2036f86d2b1aa2c
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-21 15:48:10 +0530

    is-iterable-empty

--- is-iterable-empty/isIterableEmpty.js
+++ is-iterable-empty/isIterableEmpty.js
@@ -1,6 +1,8 @@
 
-function isIterableEmpty(...args) {
-  return args;
+function isIterableEmpty(maybeEmpty) {
+  const iterator = maybeEmpty[Symbol.iterator]();
+  const hasNextValue = iterator.next().value !== undefined;
+  return !hasNextValue;
 }
 
 export {
