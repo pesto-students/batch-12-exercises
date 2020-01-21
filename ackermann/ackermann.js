de5226a0661ce984310e80002f3eea22139a3063
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-21 20:48:31 +0530

    ackermann done

--- ackermann/ackermann.js
+++ ackermann/ackermann.js
@@ -1,6 +1,22 @@
 
-function ackermann(...args) {
-  return args;
+function ackermann(object) {
+  let { m: x, n: y } = object;
+  if (x === 0) {
+    return y + 1;
+  }
+  if (y === 0) {
+    return ackermann({
+      m: x - 1,
+      n: 1,
+    });
+  }
+  return ackermann({
+    m: x - 1,
+    n: ackermann({
+      m: x,
+      n: y - 1,
+    }),
+  });
 }
 
 export {
