5657df3c11d5c75427d437e35843bddde9236d06
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-21 21:47:16 +0530

    aperture

--- aperture/aperture.js
+++ aperture/aperture.js
@@ -1,6 +1,15 @@
 
-function aperture(...args) {
-  return args;
+const splice = (start, end, array) => {
+  return array.slice(start, end);
+}
+
+function aperture(chunkSize, array) {
+  const result = [];
+  for (let index = 0; index <= array.length - chunkSize; index += 1) {
+    const chunk = splice(index, index + chunkSize, array)
+    result.push(chunk);
+  }
+  return result;
 }
 
 export {
