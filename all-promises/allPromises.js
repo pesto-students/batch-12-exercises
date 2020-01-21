be50b7ef3ff73821c101a3a5f05e4c931c9bdb51
ColdFire <fanilgandhi@gmail.com>
2020-01-21 06:45:23 +0530

    [WIP] allPromises

--- all-promises/allPromises.js
+++ all-promises/allPromises.js
@@ -1,6 +1,10 @@
 
-function allPromises(...args) {
-  return args;
+const allPromises = (array) => {
+  return Promise.resolve(function() {
+    return array.map((v) => {
+      return Promise.resolve((v) => v);
+    });
+  }());
 }
 
 export {
