68981a205162f81f569202037d426750083760c8
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-21 15:58:44 +0530

    partial done

--- consumable-users/ConsumableUsers.js
+++ consumable-users/ConsumableUsers.js
@@ -1,6 +1,24 @@
 
-function consumableUsers(...args) {
-  return args;
+function consumableUsers() {
+  this.userObject = [
+    { user: 'Alice' },
+    { user: 'Bob' },
+  ];
+  let iterationCount = 0;
+  return {
+    nextUser: function(x) {
+      ++iterationCount;
+      return {
+        value: this.userObject[iterationCount],
+        done: false
+      }
+    },
+    done: function() {
+      return {
+        done: this.userObject.user === 'Bob',
+      }
+    }
+  };
 }
 
 export {
