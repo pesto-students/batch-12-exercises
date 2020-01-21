be70940cfb7793df0278486dfad1100575d96d1c
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-21 23:43:53 +0530

    animal-sort done

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,6 +1,30 @@
 
-function animalSort(...args) {
-  return args;
+function animalSort(arrayOfAnimalObj) {
+  if (arrayOfAnimalObj.length === 0) {
+    return [];
+  }
+
+  const sortedAnimalArray = arrayOfAnimalObj.sort((animalObj1, animalObj2) => {
+    if (animalObj1.numberOfLegs < animalObj2.numberOfLegs) {
+      return -1;
+    }
+    if (animalObj1.numberOfLegs > animalObj2.numberOfLegs) {
+      return 1;
+    }
+    if (animalObj1.numberOfLegs === animalObj2.numberOfLegs) {
+      const animalObj1nameUppercased = animalObj1.name.toUpperCase();
+      const animalObj2nameUppercased = animalObj2.name.toUpperCase();
+      if (animalObj1nameUppercased < animalObj2nameUppercased) {
+        return -1;
+      }
+      if (animalObj1nameUppercased > animalObj2nameUppercased) {
+        return 1;
+      }
+      return 0;
+    }
+  });
+
+  return sortedAnimalArray;
 }
 
 export {
