8dc72817208c61361db246723594ed2e9958b2c4
ColdFire <fanilgandhi@gmail.com>
2020-01-21 07:51:37 +0530

    Complete perfectSeaures

--- sum-of-perfect-squares/sumOfPerfectSquares.js
+++ sum-of-perfect-squares/sumOfPerfectSquares.js
@@ -1,36 +1,33 @@
 
-function perfectSquareGenerator() {
-  let currentNumber = 0;
-  return (() => {
-    currentNumber += 1;
-    return currentNumber ** 2;
-  });
-}
+const isSquare = (value) => Math.floor(value ** 0.5) === (value ** 0.5);
 
-function getPerfectSquaresUpto(limit) {
-  const perfectSquaresUpto = [];
-  const getNextPerfectSquare = perfectSquareGenerator();
-  let nextPerfectSquare = getNextPerfectSquare();
-  while (nextPerfectSquare < limit) {
-    perfectSquaresUpto.push(nextPerfectSquare);
-    nextPerfectSquare = getNextPerfectSquare();
+function sumOfPerfectSquares(value) {
+  // There can only be 4 possible solutions -> 1,2,3,4
+  // Check if perfect square , then return 1
+  if (isSquare(value)) {
+    return 1;
   }
-  return perfectSquaresUpto;
-}
 
-function sumOfPerfectSquares(limit) {
-  const perfectSquares = getPerfectSquaresUpto(limit);
-  let requiredPerfectSquares = 0;
-  let i = limit;
-  while ((i > 0) && (perfectSquares.length > 0)) {
-    if (perfectSquares[-1] > i) {
-      perfectSquares.pop();
-    } else {
-      requiredPerfectSquares += 1;
-      i -= perfectSquares[-1];
+  // Manually checking for result 2
+  for (let i = 0; i <= value; i += 1) {
+    // if x=n-i*i   and x is a valid square then OBVIOUSLY
+    // n=i^2 +sqrt(x)^2  ,so n is a square of two numbers
+    if (isSquare(value - i * i)) {
+      return 2;
     }
   }
-  return requiredPerfectSquares;
+
+  // Legendre's three square theorem: A natural number n can be represented as
+  // the sum of three squares of integers if and only if : n!= 4^x ( 8*m+7)
+  let i = value;
+  while ((i % 4) === 0) {
+    i /= 4;
+  }
+  if (i % 8 === 7) {
+    return 4;
+  }
+  // otherwise
+  return 3;
 }
 
 export {
