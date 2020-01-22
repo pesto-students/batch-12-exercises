a7a4adcedbc4112c37e3486fa65bb000581aff78
ColdFire <fanilgandhi@gmail.com>
2020-01-22 15:59:08 +0530

    Complete KnownProp

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,7 +1,15 @@
+const hasProperty = (target, key) => Object.prototype.hasOwnProperty.call(target, key);
+const handler = {
+  get: (target, key) => {
+    if (hasProperty(target, key)) {
+      throw new TypeError(`Unknown property: ${key}`);
+    } else {
+      return Reflect.get(target, key);
+    }
+  },
+};
 
-function knownProp(...args) {
-  return args;
-}
+const knownProp = (target) => (new Proxy(target, handler));
 
 export {
   knownProp,
