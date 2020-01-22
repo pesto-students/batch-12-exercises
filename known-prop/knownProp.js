47154e175ac993f6636a35de7d6e5c307838675c
Viral Ruparel <viral@sysbioz.com>
2020-01-22 16:03:56 +0530

    knownProp done

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,7 +1,13 @@
 
-function knownProp(...args) {
-  return args;
-}
+const knownProp = (target) => new Proxy(target, {
+  get(_, key) {
+    if (key in target) {
+      return Reflect.get(target, key);
+    }
+    const e = new TypeError('/Unknown property/');
+    throw e;
+  },
+});
 
 export {
   knownProp,
