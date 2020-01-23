f0c9e08129d679babb47a5c7dc1e6a3541164d09
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-23 21:53:07 +0530

    bizarre-string-incrementer

--- bizarre-string-incrementer/bizarreStringIncrementer.js
+++ bizarre-string-incrementer/bizarreStringIncrementer.js
@@ -1,6 +1,27 @@
 
-function bizarreStringIncrementer(...args) {
-  return args;
+const collectNumbersTillChar = (result, value) => {
+  if (!Object.keys(result).includes('flag')) { result.flag = true; }
+  if (!isNaN(Number(value)) && result.flag === true) {
+    result.number.push(value);
+  } else {
+    result.flag = false;
+  }
+  return result;
+}
+
+
+function bizarreStringIncrementer(word) {
+  const chars = word.split('');
+  const { number } = chars.reduceRight(collectNumbersTillChar, { number: [] });
+  if (number.length === 0) {
+    return word + '1';
+  }
+  const incrementedNumber = Number(number.reverse().join('')) + 1;
+  const postFix = incrementedNumber.toString().padStart(number.length, "0");
+  const preFix = word.substring(0, word.length - number.length);
+  return preFix + postFix;
+
+
 }
 
 export {
