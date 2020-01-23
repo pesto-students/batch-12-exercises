3a7f028b92f677f42745312d1c8bbd20ced337d2
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-23 22:02:26 +0530

    bizarre-string-incrementer done

--- bizarre-string-incrementer/bizarreStringIncrementer.js
+++ bizarre-string-incrementer/bizarreStringIncrementer.js
@@ -1,7 +1,39 @@
-
-function bizarreStringIncrementer(...args) {
-  return args;
+function padZeroToString(numofZero, str) {
+  if (numofZero === 0) {
+    return str;
+  }
+  let i = 0;
+  let string = '';
+  while (i < numofZero) {
+    string = `${string}0`;
+    i += 1;
+  }
+  return `${string}${str}`;
 }
+
+
+function bizarreStringIncrementer(string) {
+  const reverseCharArray = string.split('').reverse();
+  let isFilterEnd = true;
+  const trailingNumberList = reverseCharArray.filter((value) => {
+    if (!isNaN(Number(value)) && isFilterEnd) {
+      return true;
+    }
+    isFilterEnd = false;
+  });
+  
+  const slicedCharArray = reverseCharArray.slice(trailingNumberList.length, reverseCharArray.length);
+  const charArray = slicedCharArray.reverse().join('');
+
+  const numberListToString = trailingNumberList.reverse().join('');
+
+  const incrementedNumber = Number(numberListToString) + 1;
+  const noOfZeroToPad = numberListToString.length - (String(incrementedNumber)).length;
+  const paddedString = padZeroToString(noOfZeroToPad, String(incrementedNumber));
+
+  return charArray + paddedString;
+}
+
 
 export {
   bizarreStringIncrementer,
