2a313fc2bc25f6c019f20baddc0b4ee288653e2f
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-25 01:18:37 +0530

    animation

---
+++ animation/animation.test.js
@@ -0,0 +1,8 @@
+describe('Loader has been done', () => {
+    it('1. Bar loader', () => {
+        expect(true).toBe(true);
+    })
+    it('1. Circular loader', () => {
+        expect(true).toBe(true);
+    })
+})
