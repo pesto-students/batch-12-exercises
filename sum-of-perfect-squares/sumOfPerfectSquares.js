b31b4b1abb1c156c7ae51ddb0631cd79387e5117
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-25 04:22:18 +0530

    sum of perfect square partial done

--- sum-of-perfect-squares/sumOfPerfectSquares.js
+++ sum-of-perfect-squares/sumOfPerfectSquares.js
@@ -1,24 +1,29 @@
-const smallestlistofPerfectSquare = [];
-
-function arrayofsmallsquare(number) {
-  if (number === 1) {
-    smallestlistofPerfectSquare.push(1);
-    return smallestlistofPerfectSquare;
+function isPerfectSquare(number) {
+  if (number < 1) {
+    return false;
   }
-  let temp = Math.floor(Math.sqrt(number));
-  smallestlistofPerfectSquare.push(temp);
-  var diff = number - (temp * temp);
-  return arrayofsmallsquare(diff);
+  return Number.isInteger(Math.sqrt(number));
 }
 
 function sumOfPerfectSquares(number) {
-  arrayofsmallsquare(number);
-  return smallestlistofPerfectSquare.length;
+  if (isPerfectSquare(number)) {
+    return 1;
+  }
+  let arrayOfPerfectSquares = [];
+  for (let i = 1; i < number; i += 1) {
+    if (isPerfectSquare(i)) {
+      arrayOfPerfectSquares.push(i);
+    }
+  }
+  console.log(arrayOfPerfectSquares);
+  
 }
 
-export {
-  sumOfPerfectSquares,
-};
+// export {
+//   sumOfPerfectSquares,
+// };
 
-
+console.log(sumOfPerfectSquares(19));
 // console.log(sumOfPerfectSquares(15));
+// console.log(sumOfPerfectSquares(15));
+// console.log(sumOfPerfectSquares(18));
