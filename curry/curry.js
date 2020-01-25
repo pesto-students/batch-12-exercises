24e4ac29212d1a8c5097b94827bd727f1fe821fc
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-25 23:51:36 +0530

    curry

--- curry/curry.js
+++ curry/curry.js
@@ -1,6 +1,15 @@
 
-function curry(...args) {
-  return args;
+
+function curry(func) {
+  return function curried(...args){
+    if(args.length >= func.length){
+      return func.call(null,...args)
+    }else{
+      return function(...args2){
+        return curried.call(null, ...args.concat(...args2));
+      }
+    }
+  }
 }
 
 export {
