e2e6604b8d8e2529e777d776b465f4420269ea92
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-26 17:56:56 +0530

    WIP

--- memoize/memoize.js
+++ memoize/memoize.js
@@ -1,8 +1,27 @@
 
-function memoize(...args) {
-  return args;
+function memoize(fn) {
+  const cache = {};
+  // console.log(...args);
+  return function(...args) {
+    if (Object.keys(cache).includes(args.toString())) {
+      return cache[args];
+    } else {
+      let response = fn(...args);
+      cache[args] = response
+      return response;
+    }
+  };
 }
 
-export {
-  memoize,
-};
+// export {
+//   memoize,
+// };
+
+let called = 0;
+const fib = memoize((n) => {
+  called += 1;
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
+});
+fib(10);
+console.log(called);
