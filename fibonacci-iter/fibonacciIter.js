d24d747e651b905efb26b50f6e0f4a9521924cf7
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-27 01:58:11 +0530

    WIP

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -1,7 +1,23 @@
 
-function fibonacciIter(...args) {
-  return args;
-}
+const fibonacciIter = {
+  [Symbol.iterator]() {
+    let prevValue = -1;
+    let curvalue = 1;
+    const iterable = {
+      next() {
+        while (true) {
+          prevValue = curvalue;
+          curvalue = curvalue + prevValue;
+          return {
+            value: curvalue,
+            done: false,
+          };
+        }
+      },
+    };
+    return iterable;
+  }
+};
 
 export {
   fibonacciIter,
--- generator-iterable/generatorIterable.js
+++ generator-iterable/generatorIterable.js
@@ -1,6 +1,10 @@
 
-function generatorIterable(...args) {
-  return args;
+function* generatorIterable() {
+  let num = 1;
+  while (num <= 5) {
+    yield num;
+    num += 1;
+  }
 }
 
 export {
