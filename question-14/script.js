168a7d4d2de84c8303b3560c7b1e8cf3e26b97ea
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-27 21:12:11 +0530

    question 14 done

---
+++ question-14/script.js
@@ -0,0 +1,27 @@
+window.onscroll = function() {
+    console.log(window.scrollY);
+    console.log(document.body.offsetHeight);
+    console.log(window.innerHeight, 'innerheight');
+    if((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        showLoader();
+        setTimeout(() => {
+            hideLoader();
+            addboxes();
+        }, 2000);
+    }
+}
+
+function showLoader() {
+    document.getElementById('Loader').style.display = 'block';
+}
+function hideLoader() {
+    document.getElementById('Loader').style.display = 'none';
+}
+function addboxes() {
+    for(let i = 1; i < 6; i++) {
+        let div = document.createElement('div');
+        div.className = `box${i}`;
+        document.getElementById('boxContainer').appendChild(div);
+    }
+    
+}
