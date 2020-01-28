6ae19c06acec795b378c01c78c56b5b41ec7d4e1
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-28 16:01:27 +0530

    Duplicate-letters

--- duplicate-letters/duplicateLetters.js
+++ duplicate-letters/duplicateLetters.js
@@ -1,6 +1,24 @@
 
-function duplicateLetters(...args) {
-  return args;
+function duplicateLetters(word) {
+  const duplicateMemo = {};
+  let max = 0;
+  for(const char of word){
+    let countOfChar = 1;
+    if(duplicateMemo[char] !== undefined){
+      countOfChar = duplicateMemo[char];
+      countOfChar+=1;
+      if(countOfChar > max){
+        max = countOfChar;
+      }
+    }
+    duplicateMemo[char] = countOfChar;
+  }
+
+  if(max === 0){
+    return false;
+  }else{
+    return max;
+  }
 }
 
 export {
