92cb7b236db11ef2e0935c95df2b870aad8c4e89
Viral Ruparel <viral@sysbioz.com>
2020-01-30 03:01:53 +0530

    question 3 done

---
+++ question-3/jsFile.js
@@ -0,0 +1,8 @@
+function getAttributes() {
+    var aDetials = document.getElementById('w3r');
+    console.log(aDetials.getAttribute("href"));
+    console.log(aDetials.getAttribute("type"));
+    console.log(aDetials.getAttribute("hreflang"));
+    console.log(aDetials.getAttribute("rel"));
+    console.log(aDetials.getAttribute("target"));
+}
