285330afd319e3f72ab80af935633469ff639a25
Vineet Srivastav <vineetvk01@gmail.com>
2020-01-29 15:16:57 +0530

    Stop-Watch

--- intermediate-exercises/src/components/02-StopWatch.js
+++ intermediate-exercises/src/components/02-StopWatch.js
@@ -1,26 +1,62 @@
 import React, { Component } from 'react';
 
-/*
-* Exercise 2:
-*
-*  Create a `StopWatch` component that has a Start/Stop button and a Clear
-*  button.
-*
-*  Pressing Start will start a timer and the lapsed time in
-*  milliseconds should be displayed above the buttons.
-*
-*  Once started the Start button should change to Stop. Clicking Stop
-*  will stop the timer but lapsed time will be preserved.
-*
-*  Clicking Start again will resume the timer from where it left off.
-*
-*  Clicking Clear will stop the timer if it's running and reset the lapsed time to 0.
-*/
-
 class StopWatch extends Component {
+  constructor() {
+    super();
+    this.state = {
+      timer: 0,
+      running: false,
+      timerId: undefined,
+    }
+  }
+
+  increaseByOneMilSecond = () => {
+    let { timer } = this.state;
+    timer += 1;
+    this.setState({
+      timer,
+    })
+  }
+
+  toggleTimer = (e) => {
+    const { running } = this.state;
+    if (running) {
+      this.stop();
+    } else {
+      const timerId = setInterval(this.increaseByOneMilSecond, 10);
+      this.setState({ timerId: timerId, running: true });
+    }
+  }
+
+  stop = (e) => {
+    console.log("Timer stop");
+    this.setState({ running: false });
+    const { timerId, running } = this.state;
+    console.log("Running: " + running);
+    clearInterval(timerId);
+  }
+
+  clear = (e) => {
+    this.setState({
+      timer: 0,
+      running: false,
+      timerId: undefined,
+    })
+  }
+
+  pad = (d) => {
+    return (d < 10) ? '0' + d.toString() : d.toString();
+  }
+
   render() {
+    const { timer, running } = this.state;
     return (
-      <div>Stop Watch</div>
+      <React.Fragment>
+        <div>Stop Watch</div>
+        <h3 id="watch"> {(parseInt(timer / 100))} Minutes : {(this.pad(timer % 100))} Seconds</h3>
+        <button onClick={this.toggleTimer}>{running ? "Stop" : "Start"}</button>
+        <button onClick={!running?this.clear:null}>Clear</button>
+      </React.Fragment>
     );
   }
 }
