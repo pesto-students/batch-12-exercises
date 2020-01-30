a6bdbe02276c1fe225c1223bf556a2f7896e2693
nitish mittal <nitish.mittal@orchids.edu.in>
2020-01-30 15:55:04 +0530

    Router Done

--- setup-router/src/App.js
+++ setup-router/src/App.js
@@ -1,20 +1,31 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
 
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
-import './styles/App.css';
+import "./styles/App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <Navbar />
-        <Home />
-        <About />
-        <Contact />
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/aboutus">
+              <About />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
+        </Router>
       </Fragment>
     );
   }
