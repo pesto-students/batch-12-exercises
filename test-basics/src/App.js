ff430142d26c52f704ccb9c5ffdc4f968c88767c
ColdFire <fanilgandhi@gmail.com>
2020-02-01 00:34:44 +0530

    Completed test-basics

--- test-basics/src/App.js
+++ test-basics/src/App.js
@@ -1,26 +1,64 @@
 import React, { Component } from 'react';
-
 import './styles/App.css';
 
-/* eslint-disable react/no-multi-comp, react/no-unused-state */
-class App extends Component {
-  state = {
-    on: false,
-    input: '',
-    mainColor: 'blue',
+const isTextOrEmpty = (value) => Boolean(value) ? value.toString() : '' 
+
+export class Link extends Component {
+  constructor(props) {
+    super(props);
   }
+
   render() {
-    return (
-      <div className="App">
-        Welcome to React
-      </div>
-    );
+    if (this.props.hide !== true) {
+      return <a href={this.props.address}></a>;
+    } else {
+      return null;
+    }
+  
   }
 }
 
-export class Link extends Component {
+
+/* eslint-disable react/no-multi-comp, react/no-unused-state */
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      on: false,
+      input: '',
+      mainColor: 'blue',
+      currentTarget : '',
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.handleStrings = this.handleStrings.bind(this);
+  }
+
+  handleInputChange(event) {
+    const inputText = (event.target !== undefined) ? event.target.value : '';
+    this.setState({ currentTarget: isTextOrEmpty(inputText) });
+  }
+
+  handleButtonClick() {
+    this.setState({ on: !this.state.on });
+  }
+
+  handleStrings(inputString) {
+    return Boolean(inputString);
+  }
+
   render() {
-    return null;
+    return (
+      <div className="App">
+        <h1>Welcome to React</h1>
+        <h2 className={this.state.mainColor}>{this.state.currentTarget}</h2>
+        <button className="button" onClick={this.handleButtonClick}>Change color</button>
+        <p className="button-state">{this.state.on ? 'Yes!' : 'No!'}</p>
+        <input type="text" onChange={this.handleInputChange}></input>
+        <link></link>
+      </div>
+    );
   }
 }
 
--- test-basics/src/setupTests.js
+++ test-basics/src/setupTests.js
@@ -1,4 +1,4 @@
-import { configure } from 'enzyme';
+import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-configure({ adapter: Adapter });
+Enzyme.configure({ adapter: new Adapter() });
