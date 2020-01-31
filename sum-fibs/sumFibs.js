bd69b7cca49efcb914c77511a631c96ecc925d71
ColdFire <fanilgandhi@gmail.com>
2020-02-01 02:16:40 +0530

    Completed Sum-Fibs

--- sum-fibs/sumFibs.js
+++ sum-fibs/sumFibs.js
@@ -1,28 +1,29 @@
 
-function fibonacciGenerator() {
-  let a = 0;
-  let b = 1;
-  return (() => {
-    b += a;
-    a = b - a;
-    return a;
-  });
+function* fibonacciGenerator() {
+  let firstFibonacci = 0;
+  let secondFibonacci = 1;
+  while (true) {
+    firstFibonacci += secondFibonacci;
+    secondFibonacci = firstFibonacci - secondFibonacci;
+    yield firstFibonacci;
+  }
 }
 
-function getOddFibonaciiUpto(uptoNumber) {
-  const fibonacciNumbers = [];
-  const getNextFibonaci = fibonacciGenerator();
-  let nextFibonacci = getNextFibonaci();
+function getOddFibonacciUpto(uptoNumber) {
+  const oddFibonacciNumbers = [];
+  const getFibonacci = fibonacciGenerator();
+  let nextFibonacci = getFibonacci.next().value;
   while (nextFibonacci < uptoNumber) {
-    fibonacciNumbers.push(nextFibonacci);
-    nextFibonacci = getNextFibonaci();
+    if ((nextFibonacci % 2) !== 0) {
+      oddFibonacciNumbers.push(nextFibonacci);
+    }
+    nextFibonacci = getFibonacci.next().value;
   }
-  const oddFibonacciNumbers = fibonacciNumbers.filter((n) => (n % 2) !== 0);
   return oddFibonacciNumbers;
 }
 
 function sumFibs(uptoNumber) {
-  const oddFibonacciNumbers = getOddFibonaciiUpto(uptoNumber);
+  const oddFibonacciNumbers = getOddFibonacciUpto(uptoNumber);
   const sumFibonacciNumbers = oddFibonacciNumbers.reduce((a, b) => (a + b), 0);
   return sumFibonacciNumbers;
 }
