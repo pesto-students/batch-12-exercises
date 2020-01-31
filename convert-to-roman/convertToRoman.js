566f1f26978c3e7e7b03592c54f3f624ead5523c
ColdFire <fanilgandhi@gmail.com>
2020-02-01 03:46:02 +0530

    [WIP] Convert-to-Roman

--- convert-to-roman/convertToRoman.js
+++ convert-to-roman/convertToRoman.js
@@ -1,6 +1,26 @@
 
-function convertToRoman(...args) {
-  return args;
+const romanValues = {
+  'M': 1000,
+  'D': 500,
+  'C': 100,
+  'L': 50,
+  'X': 10,
+  'V': 5,
+  'I': 1
+}
+
+function convertToRoman(number) {
+  const stringifiedNumber = Array.from(number.toString());
+  stringifiedNumber.map((value, index) => {
+    const multiplier = 10 ** (index + 1);
+    if (value === 0) {
+      return '';
+    }
+    if ((value === 1) || (value === 2) || (value === 3)) {
+      return value.repeat(multiplier);
+    }
+    
+  });
 }
 
 export {
