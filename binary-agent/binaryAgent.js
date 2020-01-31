82df37d127b2694f16072b282a4e6ad69356d36e
ColdFire <fanilgandhi@gmail.com>
2020-02-01 03:02:55 +0530

    Completed BinaryAgent

--- binary-agent/binaryAgent.js
+++ binary-agent/binaryAgent.js
@@ -1,6 +1,9 @@
 
-function binaryAgent(...args) {
-  return args;
+function binaryAgent(binaryString) {
+  const binaryChars = binaryString.split(' ');
+  const asciiChars = binaryChars.map((value) => parseInt(value, 2));
+  const string = asciiChars.map((value) => String.fromCharCode(value)).join('');
+  return string;
 }
 
 export {
