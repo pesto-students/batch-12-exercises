b27f682f9a86ecb882c3415fd38039fa8c7f5b49
Viral Ruparel <viral@sysbioz.com>
2020-01-31 14:51:00 +0530

    setup router wip

--- setup-router/src/App.js
+++ setup-router/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component, Fragment } from 'react';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
@@ -10,12 +14,19 @@ import './styles/App.css';
 class App extends Component {
   render() {
     return (
+
       <Fragment>
         <Navbar />
-        <Home />
-        <About />
-        <Contact />
+        <Router>
+          <Switch>
+            <Route component={Home} exact pattern="/"></Route>
+            <Route component={About} pattern="/about"></Route>
+            <Route component={Contact} pattern="/contact"></Route>
+          </Switch>
+        </Router>
       </Fragment>
+
+
     );
   }
 }
