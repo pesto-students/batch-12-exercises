c194a3ff23b32e035e60bb249496a2a0acc8eca7
ColdFire <fanilgandhi@gmail.com>
2020-02-01 01:39:52 +0530

    Completed rectangle-diagonal

--- rectangle-diagonal/rectangleDiagonal.js
+++ rectangle-diagonal/rectangleDiagonal.js
@@ -1,7 +1,7 @@
 
-function rectangleDiagonal(...args) {
-  return args;
-}
+const square = (value) => value ** 2;
+const squareroot = (value) => parseFloat(value ** 0.5, 2);
+const rectangleDiagonal = (length, breadth) => squareroot(square(length) + square(breadth));
 
 export {
   rectangleDiagonal,
