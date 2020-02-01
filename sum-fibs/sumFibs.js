2b9002371a40e770fc3f8a047060b4d9e669e9b0
Viral Ruparel <viral@sysbioz.com>
2020-02-01 14:19:44 +0530

    sumFibs done

--- sum-fibs/sumFibs.js
+++ sum-fibs/sumFibs.js
@@ -1,6 +1,20 @@
-
-function sumFibs(...args) {
-  return args;
+function sumFibs(num) {
+  if (typeof num !== 'number') {
+    return false;
+  }
+  if (num < 0) return -1;
+  if (num === 0 || num === 1) return 1;
+  let prevNumber = 0;
+  let currNumber = 1;
+  let result = 0;
+  while (currNumber <= num) {
+    if (currNumber % 2 !== 0) {
+      result += currNumber;
+    }
+    currNumber += prevNumber;
+    prevNumber = currNumber - prevNumber;
+  }
+  return result;
 }
 
 export {
--- sum-of-perfect-squares/sumOfPerfectSquares.js
+++ sum-of-perfect-squares/sumOfPerfectSquares.js
@@ -3,6 +3,19 @@ function sumOfPerfectSquares(...args) {
   return args;
 }
 
+function* oddFibGenerator() {
+  let i = 0;
+  while (true) {
+
+  }
+}
+
+function fib(number) {
+  if (i < 2) {
+    return i
+  }
+  return fib(i - 1) + fib(i - 2);
+}
 export {
   sumOfPerfectSquares,
 };
