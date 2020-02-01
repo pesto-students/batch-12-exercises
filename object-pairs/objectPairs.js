47d878f9c659b307d15360d1efeeb9bac6de95a3
Viral Ruparel <viral@sysbioz.com>
2020-02-01 20:05:10 +0530

    object-pairs done

--- object-pairs/objectPairs.js
+++ object-pairs/objectPairs.js
@@ -1,6 +1,10 @@
 
-function objectPairs(...args) {
-  return args;
+function objectPairs(object) {
+  const resultArray = [];
+  for (const [key, value] of Object.entries(object)) {
+    resultArray.push([key, value]);
+  }
+  return resultArray;
 }
 
 export {
