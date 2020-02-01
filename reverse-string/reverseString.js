5023f7edc9c304f4dc95db3ae1e813e2a3813420
ColdFire <fanilgandhi@gmail.com>
2020-02-01 15:51:27 +0530

    Completed Reverse-String

--- reverse-string/reverseString.js
+++ reverse-string/reverseString.js
@@ -1,6 +1,8 @@
 
-function reverseString(...args) {
-  return args;
+function reverseString(string) {
+  const chars = string.split('');
+  const reducer = (accumulator, currentValue) => currentValue + accumulator;
+  return chars.reduce(reducer, '');
 }
 
 export {
