6a8bef1b10518b7d39c445f3e2c22b89442fef79
Viral Ruparel <viral@sysbioz.com>
2020-02-01 22:07:49 +0530

    memoize done

--- memoize/memoize.js
+++ memoize/memoize.js
@@ -1,6 +1,14 @@
 
-function memoize(...args) {
-  return args;
+function memoize(fun) {
+  const memo = {};
+  return function innerFunction(...args) {
+    if (args in memo) {
+      return memo[args];
+    }
+    const result = fun(...args);
+    memo[args] = result;
+    return result;
+  };
 }
 
 export {
