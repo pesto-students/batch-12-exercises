8b31f2c874fabaf9c547d1e399f18736ef3ab0d8
Viral Ruparel <viral@sysbioz.com>
2020-02-02 02:52:19 +0530

    mapFilterReduce done

--- map-filter-and-reduce/mapFilterAndReduce.js
+++ map-filter-and-reduce/mapFilterAndReduce.js
@@ -1,6 +1,12 @@
 
-function mapFilterAndReduce(...args) {
-  return args;
+function mapFilterAndReduce(args) {
+  return args.map((item) => item.firstName)
+    .filter((ele) => ele.length < 5)
+    .reduce((acc, curr) => {
+      acc[curr] = curr.length;
+      return acc;
+    }, {});
+  //
 }
 
 export {
