c88efa9572544f977d707f9b15f0568fbed9c1c9
Viral Ruparel <viral@sysbioz.com>
2020-02-02 04:04:29 +0530

    promiseAllProps done

--- promise-all-props/promiseAllProps.js
+++ promise-all-props/promiseAllProps.js
@@ -1,6 +1,13 @@
 
-function promiseAllProps(...args) {
-  return args;
+function promiseAllProps(promiseDic) {
+  const promise = new Promise(() => {
+    const newDic = {};
+    for (const [key, value] of Object.entries(promiseDic)) {
+      newDic[key] = value.then((promiseValue) => promiseValue);
+    }
+    return newDic;
+  });
+  return promise;
 }
 
 export {
