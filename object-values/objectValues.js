337266b3ebe58445aeb291616a7e80ee0b7a0b37
Viral Ruparel <viral@sysbioz.com>
2020-02-01 20:06:47 +0530

    object-values done

--- object-values/objectValues.js
+++ object-values/objectValues.js
@@ -1,6 +1,10 @@
 
-function objectValues(...args) {
-  return args;
+function objectValues(object) {
+  const resultArray = [];
+  for (const [, value] of Object.entries(object)) {
+    resultArray.push(value);
+  }
+  return resultArray;
 }
 
 export {
