a7ad33e218bdf3c69a4fc6f9954842597fe13996
Viral Ruparel <viral@sysbioz.com>
2020-02-02 03:20:29 +0530

    placeInMiddle done

--- place-in-middle/placeInMiddle.js
+++ place-in-middle/placeInMiddle.js
@@ -1,6 +1,7 @@
 
-function placeInMiddle(...args) {
-  return args;
+function placeInMiddle(array1, array2) {
+  const pointOfInsertion = Math.floor(array1.length / 2);
+  return array1.slice(0, pointOfInsertion).concat(array2).concat(array1.slice(pointOfInsertion));
 }
 
 export {
