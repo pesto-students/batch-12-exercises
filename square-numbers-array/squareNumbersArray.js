1a224b247dafa990282af10204a7ee88dfa7879e
nitish mittal <nitish.mittal@orchids.edu.in>
2020-02-02 03:17:05 +0530

    square number array done

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,6 +1,10 @@
 
-function squareNumbersArray(...args) {
-  return args;
+function squareNumbersArray(NumberArray) {
+  const isNumberArray = NumberArray.every((eachnumber) => typeof eachnumber === 'number');
+  if (!isNumberArray) {
+    throw new Error('Please ensure all elements of array are number');
+  }
+  return NumberArray.map((number) => number * number);
 }
 
 export {
