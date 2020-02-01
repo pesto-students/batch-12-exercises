6a53433d8f0dd331087d34094964e84292b70651
Viral Ruparel <viral@sysbioz.com>
2020-02-01 15:34:36 +0530

    sum-primes done

--- sum-of-perfect-squares/sumOfPerfectSquares.js
+++ sum-of-perfect-squares/sumOfPerfectSquares.js
@@ -3,19 +3,7 @@ function sumOfPerfectSquares(...args) {
   return args;
 }
 
-function* oddFibGenerator() {
-  let i = 0;
-  while (true) {
 
-  }
-}
-
-function fib(number) {
-  if (i < 2) {
-    return i
-  }
-  return fib(i - 1) + fib(i - 2);
-}
 export {
   sumOfPerfectSquares,
 };
