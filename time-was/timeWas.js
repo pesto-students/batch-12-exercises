0c7d954e42fda97aaf5948c93c5c4558ed65c6bb
nitish mittal <nitish.mittal@orchids.edu.in>
2020-02-02 20:24:40 +0530

    time-was done

--- time-was/timeWas.js
+++ time-was/timeWas.js
@@ -1,6 +1,44 @@
+function formattedTimePassed(durationPassed, time, timeType) {
+  const timeTypePassed = durationPassed / time;
+  return timeTypePassed > 1
+    ? `${timeTypePassed} ${timeType} ago`
+    : `${timeTypePassed} ${timeType.substring(0, timeType.length - 1)} ago`;
+}
 
-function timeWas(...args) {
-  return args;
+function timeWas(currentTime, now) {
+  const NOW = now || 1516816290;
+  const SECONDS = 1000;
+  const MINUTES = SECONDS * 60;
+  const HOURS = MINUTES * 60;
+  const DAYS = HOURS * 24;
+  const WEEKS = DAYS * 7;
+  const MONTHS = DAYS * 30;
+  const YEARS = MONTHS * 12;
+
+  const durationPassed = NOW - currentTime;
+
+  if (durationPassed < SECONDS) {
+    return 'just now';
+  }
+  if (durationPassed < MINUTES) {
+    return formattedTimePassed(durationPassed, SECONDS, 'seconds');
+  }
+  if (durationPassed < HOURS) {
+    return formattedTimePassed(durationPassed, MINUTES, 'minutes');
+  }
+  if (durationPassed < DAYS) {
+    return formattedTimePassed(durationPassed, HOURS, 'hours');
+  }
+  if (durationPassed < WEEKS) {
+    return formattedTimePassed(durationPassed, DAYS, 'days');
+  }
+  if (durationPassed < MONTHS) {
+    return formattedTimePassed(durationPassed, WEEKS, 'weeks');
+  }
+  if (durationPassed < YEARS) {
+    return formattedTimePassed(durationPassed, MONTHS, 'months');
+  }
+  return formattedTimePassed(durationPassed, YEARS, 'years');
 }
 
 export {
