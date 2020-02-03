80c0955fec921e0e367b79957f9d88d78444c48f
ColdFire <fanilgandhi@gmail.com>
2020-02-03 14:53:36 +0530

    [WIP] Completed lastIndexOf

--- last-index-of/lastIndexOf.js
+++ last-index-of/lastIndexOf.js
@@ -1,7 +1,14 @@
 
-function lastIndexOf(...args) {
-  return args;
-}
+const lastIndexOf = (element, array) => {
+  let index = array.length - 1;
+  // eslint-disable-next-line
+  for (; index > -1; index += 1) {
+    if (array[index] === element) {
+      return index;
+    }
+  }
+  return index;
+};
 
 export {
   lastIndexOf,
