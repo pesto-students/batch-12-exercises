40fba857f8b8ac3b3c6742df359d8dcf655e9721
ColdFire <fanilgandhi@gmail.com>
2020-02-06 15:34:23 +0530

    Completed SplitLines

--- split-lines/splitLines.js
+++ split-lines/splitLines.js
@@ -1,8 +1,16 @@
 
-function splitLines(...args) {
-  
-  return args;
-}
+const splitLines = (lines, { preserveNewlines } = { preserveNewLines: false }) => {
+  const linesArray = lines.split('\n');
+  if (preserveNewlines === true) {
+    return linesArray.map((value, index, array) => {
+      if (index !== array.length - 1) {
+        return `${value}\n`;
+      }
+      return value;
+    });
+  }
+  return linesArray.map((line) => line.trim());
+};
 
 export {
   splitLines,
