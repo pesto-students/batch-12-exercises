5b9524e4013c4aae15dd1c3ca9fcd8b17e20e385
ColdFire <fanilgandhi@gmail.com>
2020-02-06 20:00:42 +0530

    [WIP] Completed matcher

--- matcher/matcher.js
+++ matcher/matcher.js
@@ -1,7 +1,18 @@
 
-function matcher(...args) {
-  return args;
+function matcher(stringArray, patternArray) {
+  const mergedRegex = `/(${patternArray.join(")|(")})/`;
+  console.log(mergedRegex);
+  function isValidString(string) {
+    return Boolean(string.match(mergedRegex));
+  }
+  return isValidString(stringArray[0]);
+  // return stringArray.filter(isValidString);
 }
+
+
+console.log(matcher(['foo', 'bar'], ['f*o']));
+
+
 
 export {
   matcher,
