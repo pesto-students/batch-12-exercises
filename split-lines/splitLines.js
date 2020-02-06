4c8811d4375a5eb83bdbe571bc0e9285c9be990d
Vineet Srivastav <vineetvk01@gmail.com>
2020-02-07 02:28:25 +0530

    Split-lines

--- split-lines/splitLines.js
+++ split-lines/splitLines.js
@@ -1,6 +1,17 @@
 
-function splitLines(...args) {
-  return args;
+function splitLines(paragraph, preserveNewlinesObj = {preserveNewlines:false} ) {
+  let {preserveNewlines} = preserveNewlinesObj;
+  
+  const paragraphWithSpacesAtNewLine = paragraph.split("\n").join("\n ");
+  const wordsWithNewLine = paragraphWithSpacesAtNewLine.split(" ");
+  
+  if(preserveNewlines === false){
+    const pureWords = wordsWithNewLine.map(word=>{
+      return word.replace(/[\r]/,"").replace(/[\n]/,"");
+    })
+    return pureWords;
+  }
+  return wordsWithNewLine;
 }
 
 export {
